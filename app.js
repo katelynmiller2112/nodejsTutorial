7e30bf7ae4c25d30b8162926a81782a107b3b0b1
Miller <millerk9@wit.edu>
2021-11-09 06:03:51 -0500

    contuining nodejs work learning about intervals

--- app.js
+++ app.js
@@ -14,3 +14,16 @@ app.listen(3000, () => {
     console.log("Listening to 3000");
 });
 
+
+setTimeout(() => {
+    console.log('in the timeout');
+    clearInterval(int);
+},3000);
+
+const int = setInterval(() => {
+    console.log('in the interval');
+},1000)
+
+
+console.log(__dirname);
+console.log(__filename);
